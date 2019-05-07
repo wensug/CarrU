0d237139442245084fc135f41988704e1978f855
Gamile Mustapha <gamilemustapha@android-e29e85de1b3c84a9.localdomain>
2019-05-07 16:46:45 +0100

    footer

---
+++ public/footerComponent.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Footer (props) {
+    return (
+        <footer>
+            <div>
+                <ul>
+                    <li><Link>Home</Link></li>
+                    <li><Link>Events</Link></li>
+                    <li><Link>Register</Link></li>
+                    <li><Link>Create Event</Link></li>
+                    <li><Link>Login</Link></li>
+                </ul>
+            </div>
+        </footer>
+        );
+    
+}
+
+
+export default Footer;
+
