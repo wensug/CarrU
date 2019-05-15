52049a9e127434027b73eb401802797177076371
LuciaCL <luciacoucelamas@gmail.com>
2019-05-15 22:17:53 +0100

    fake data (#24)

---
+++ src/frontend/data/events-get.js
@@ -0,0 +1,7 @@
+export default function getEvents(){
+    return[
+        {id:'1', name:'Game in Manchester', date:'09-06-2019',group:'1', users:['1', '2']},
+        {id:'2', name:'Game in London',date:'15-06-2019', group:'3', users:['5', '6', '8']},
+        {id:'3', name:'Birthday Mary', date:'16-06-2019',group:'4', users:['1', '2', '7', '8']},
+        ];
+} 
---
+++ src/frontend/data/users-get.js
@@ -0,0 +1,13 @@
+
+export default function getUsers(){
+    return[
+        {id:'1', firstName:'Jhon', lastName:'Doe', email:'JDoe@gmail.com',password:'12345'},
+        {id:'2', firstName:'Peter', lastName:'Smith', email:'smccartan@gmail.com',password:'12345'},
+        {id:'3', firstName:'Albert', lastName:'Huntington', email:'jhames@gmail.com',password:'12345'},
+        {id:'4', firstName:'Julia', lastName:'Prator', email:'Jprator@gmail.com',password:'12345'},
+        {id:'5', firstName:'Robert', lastName:'Belf', email:'Rbelf@gmail.com',password:'12345'},
+        {id:'6', firstName:'Anna', lastName:'Sadry', email:'Asadry@gmail.com',password:'12345'},
+        {id:'7', firstName:'Sara', lastName:'Petz', email:'Apetz@gmail.com',password:'12345'},
+        {id:'8', firstName:'Mary', lastName:'Ert', email:'Mert@gmail.com',password:'12345'},
+        ];
+} 
