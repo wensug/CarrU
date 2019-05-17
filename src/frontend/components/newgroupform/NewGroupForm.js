511cfe26bcdc5bd4b4062a0263afd25d3320f995
Wendy Gonzalez <46827516+wensug@users.noreply.github.com>
2019-05-17 12:11:00 +0100

    New Group Form (#31)

---
+++ src/frontend/components/newgroupform/NewGroupForm.js
@@ -0,0 +1,53 @@
+import React from 'react';
+// import the css when is ready
+export default class NewGroupForm extends React.Component { 
+    constructor(){
+        super();
+        this.state ={
+            memberList: []
+        };
+    }
+   
+    addMemberList(){
+        let member = document.getElementById('input-member').value
+        let memberList = this.state.memberList;
+        memberList.push(member);
+        this.setState({memberList: memberList});
+        document.getElementById('input-member').value = '';
+    }
+   
+    render() {
+        return ( 
+            <div>
+                 <div className='new-group'>
+                    <form>
+                        <h1>New Group</h1>
+                        <label>
+                            Group Name: 
+                            <input className='group-name'/>
+                        </label> 
+                        <label>
+                            Description: 
+                            <input className='group-description'/>
+                        </label> 
+                    </form>
+                </div>  
+                <div className='add-member'>
+                    <label> Add Member:
+                        <input id='input-member' className='new-member' placeholder='Please add the member email'></input>
+                    </label>
+                    <button className='add-memberbtn' onClick={() => this.addMemberList()}>Add</button>
+                  <list>
+                        {
+                            this.state.memberList.map(member => <p>{member}</p>)
+                        }
+                    </list>
+                </div>
+                <div>
+                    <button className='create-groupbtn'>Create</button>
+                </div>
+            </div>
+        )
+    }
+
+}
