6743e8390d0c1364fe0861fb60ef38f0fe8b0e8e
nicola g <43208029+kinolag@users.noreply.github.com>
2019-05-30 18:55:30 +0100

    Fixed div closing tag in UserProfile (#69)

--- src/frontend/components/UserProfile/UserProfile.js
+++ src/frontend/components/UserProfile/UserProfile.js
@@ -148,7 +148,6 @@ class UserProfile extends React.Component {
                             </div>
                         </div>
 
-                    </div>
                     <NavLink to='/user-profile'><button>Edit Details</button></NavLink>
                     <NavLink to='/user-dashboard'><button>Dashboard</button></NavLink>
                 </form>
