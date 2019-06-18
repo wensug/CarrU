0411f37c8cccf438f2a155b50a32ed467679b931
Davide Naccarati <davgio7@gmail.com>
2019-06-18 12:31:24 +0100

    Finalcss (#73)
    * first push to git with little fix sorted
    
    * add the styling to almost the form except for the usre profile one

--- src/frontend/components/NewGroupForm/NewGroupForm.js
+++ src/frontend/components/NewGroupForm/NewGroupForm.js
@@ -49,39 +49,43 @@ export default class NewGroupForm extends React.Component {
 
     render() {
         return (
-            <div>
+            
                 <div className='new-group'>
-                    <form>
-                        <h1>New Group</h1>
-                        <hr></hr>
-                        <label>
-                            Group Name:
-                            <input id='group-name' className='group-name' />
-                        </label>
-                        <label>
-                            Description:
-                            <input id='group-description' className='group-description' />
-                        </label>
-
-                        <div className='add-member'>
-                            <label> Add Member:
-                        <input id='input-member' className='new-member' placeholder='Please add the member email'></input>
-                            </label>
+                    <form className='form-newGroup'>
+                        <h1 className='newGroup-title'>New Group</h1>
+                        <div className='form-box'>
+                        <label>Group Name:</label>
+                        <input id='group-name' className='group-name'  placeholder='Group Name'/>
+                        </div>
+                        <div className='form-box'>
+                        <label>Description:</label>
+                        <input id='group-description' className='group-description'  placeholder='Description'/>
+                        </div>
+                        <div className='form-box'>
+                       
+                            <label>Add Member:</label>
+                            <input id='input-member' className='new-member' placeholder='Member e-mail'></input>
                             <button onClick={() => this.addMemberList()}>Add</button>
+                            
+                        
+                        </div>
+                        <div className='form-box add-member'>
                             <list>
                                 {
                                     this.state.memberList.map(member => <p>{member}</p>)
                                 }
                             </list>
                         </div>
-                        <div>
+
+                        
+                        <div className='form-box'>
                             <NavLink to='/user-dashboard'><button onClick={e => this.handleSubmit(e)}>Create</button></NavLink>
                             <button onClick={() => this.resetter()}>Reset</button>
                             <NavLink to='/user-dashboard'><button>Cancel</button></NavLink>
                         </div>
                     </form>
                 </div>
-            </div>
+            
         )
     }
 
